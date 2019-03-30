fc61fc69ddb34b653fc2ba2a0b6005726efd242f
Gianluca Guarini <gianluca.guarini@gmail.com>
2019-03-30 17:09:27 +0100

    updated: enable travis tests

--- index.next.js
+++ index.next.js
@@ -1,18 +1,18 @@
 /**
- * Performs left-to-right function composition.
- * @param   {...[function]} fns) - list of unary fynctions
+ * Similar to compose but performs from left-to-right function composition.<br/>
+ * {@link https://30secondsofcode.org/function#composeright see also}
+ * @param   {...[function]} fns) - list of unary function
  * @returns {*} result of the computation
- * @link: https://30secondsofcode.org/function#composeright
  */
 export const composeRight = (...fns) => compose(...fns.reverse())
 
 /**
- * Performs right-to-left function composition.
- * Use Array.prototype.reduce() to perform right-to-left function composition.
- * The last (rightmost) function can accept one or more arguments; the remaining functions must be unary.
- * @param   {...[function]} fns) - list of unary fynctions
+ * Performs right-to-left function composition.<br/>
+ * Use Array.prototype.reduce() to perform right-to-left function composition.<br/>
+ * The last (rightmost) function can accept one or more arguments; the remaining functions must be unary.<br/>
+ * {@link https://30secondsofcode.org/function#compose original source code}
+ * @param   {...[function]} fns) - list of unary function
  * @returns {*} result of the computation
- * @link: https://30secondsofcode.org/function#compose
  */
 export default function compose(...fns) {
   return fns.reduce((f, g) => (...args) => f(g(...args)))
