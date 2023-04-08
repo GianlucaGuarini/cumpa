c4d9963aeedae2001a718500e3f185c146b5e503
gianlucaguarini <gianluca.guarini@gmail.com>
2023-04-08 23:57:14 +0200

    updated: fix registry uploaded files

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,5 @@
-import resolve from 'rollup-plugin-node-resolve'
-
 export default {
   input: 'index.next.js',
-  plugins: [
-    resolve({
-      jsnext: true
-    })
-  ],
   output: [
     {
       name: 'cumpa',
